e84cdce8315b05931859e6c145a87febbd932658
Marcus <marcusanderberg@gmail.com>
2018-09-21 19:43:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-console.log('JavaScript');
+/*console.log('JavaScript');
 console.log(2011);
 console.log('Woohoo! I love to code! #codecademy');
 console.log(20.49);
 
 
 console.log(Math.floor(Math.random() * 100));
+*/
 
 
+//lots o cats 
 var drawCats = function(howManyTimes){
 for (var i=0;i<howManyTimes;i++){
 console.log(i + " =^.^= ");
 
 }
 }
-//drawCats(10)
+drawCats(10)
