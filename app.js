1ab2f7ddc4f37685c03f526255c41abf06f35f24
Marcus <marcusanderberg@gmail.com>
2018-09-21 19:40:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ console.log(20.49);
 
 
 console.log(Math.floor(Math.random() * 100));
+
+
+var drawCats = function(howManyTimes){
+for (var i=0;i<howManyTimes;i++){
+console.log(i + " =^.^= ");
+
+}
+}
+//drawCats(10)
